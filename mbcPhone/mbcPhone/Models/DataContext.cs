3d473f6c5a8f21ed84835e5ff5ca01619efe3aed
sergio hoyos <yeyohoyos3@gmail.com>
2019-08-24 13:45:16 -0400

    solucion completa

---
+++ mbcPhone/mbcPhone/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace mbcPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mbcPhone.Models.Phone> Phones { get; set; }
+    }
+
+}
