c58075d078c21211d8f7f68dbcdf3ef07ba15136
Carlos Camacho <carlosalcamachoca@gmail.com>
2019-10-05 13:48:09 -0400

    xamarin

---
+++ mbcPhone/appPhone/appPhone/Models/Phone.cs
@@ -0,0 +1,26 @@
+﻿namespace appPhone.Models
+{
+
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Faceboock,
+        Twitter,
+        LinkedIN
+    }
+
+
+    public class Phone
+    {
+
+        public int PhoneID { get; set; }
+
+        public string Name { get; set; }
+ 
+        public TypeContact Type { get; set; }
+
+        public string Contact { get; set; }
+
+    }
+}
